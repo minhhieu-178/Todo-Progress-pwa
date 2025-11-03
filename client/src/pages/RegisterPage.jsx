35bbd6b8236f3e697810f45429658d84c2c00705
minhhieu-178 <tahieu015@gmail.com>
2025-11-03 18:10:12 +0700

    Fix RegisterPage.gisterPagejsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import api from "../utils/api";
+import api from "../services/api";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
