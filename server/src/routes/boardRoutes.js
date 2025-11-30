12fbf4f6bfdc14d6a649c7b0e221a1421f8259cb
duchieu34 <duchieuhp2004@gmail.com>
2025-11-30 14:32:45 +0700

    thêm activity log (model, service, route)

--- server/src/routes/boardRoutes.js
+++ server/src/routes/boardRoutes.js
@@ -5,12 +5,6 @@ import {
   getBoardById,
   updateBoard,
   deleteBoard,
-<<<<<<< HEAD
-=======
-  createList,
-  updateList,
-  deleteList,
->>>>>>> 39929d12d5c05eb2b0a999378e59d851dea2c221
   addMember,
   removeMember
 } from '../controllers/boardController.js';
@@ -35,14 +29,10 @@ router.delete('/:id/members/:userId', protect, removeMember);
 // --- Route cho List (trỏ vào listController) ---
 router.post('/:boardId/lists', protect, createList);
 router.put('/:boardId/lists/:listId', protect, updateList);
-<<<<<<< HEAD
-router.delete('/:boardId/lists/:listId', protect, deleteList); // Thêm tính năng xóa list
-=======
 router.delete('/:boardId/lists/:listId', protect, deleteList);
 
 router.put('/:id/members', protect, addMember);
 // --- Thêm route Xóa thành viên ---
 router.delete('/:id/members/:userId', protect, removeMember); 
->>>>>>> 39929d12d5c05eb2b0a999378e59d851dea2c221
 
 export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -3,19 +3,10 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js'; // <--- Import User Routes
 import boardRoutes from './routes/boardRoutes.js';
 import cardRoutes from './routes/cardRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
-<<<<<<< HEAD
-import searchRoutes from './routes/searchRoutes.js';
 import logRoutes from './routes/logRoutes.js';
-=======
-<<<<<<< HEAD
-import logRoutes from './routes/logRoutes.js';
-=======
->>>>>>> 39929d12d5c05eb2b0a999378e59d851dea2c221
->>>>>>> duchieu
 import notificationRoutes from './routes/notificationRoutes.js';
 
 dotenv.config();
@@ -28,19 +19,10 @@ app.use(cors());
 app.get('/', (req, res) => res.send('API đang chạy...'));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes); // <--- Đăng ký /api/users
 app.use('/api/boards', boardRoutes);
-app.use('/api', cardRoutes);
+app.use('/api', cardRoutes); // Lưu ý route này ko có prefix /cards vì trong file route đã định nghĩa dài
 app.use('/api/comments', commentRoutes);
-<<<<<<< HEAD
-app.use('/api/search', searchRoutes);
 app.use('/api/logs', logRoutes);
-=======
-<<<<<<< HEAD
-app.use('/api/logs', logRoutes);
-=======
->>>>>>> 39929d12d5c05eb2b0a999378e59d851dea2c221
->>>>>>> duchieu
 app.use('/api/notifications', notificationRoutes);
 
 const PORT = process.env.PORT || 5001;
